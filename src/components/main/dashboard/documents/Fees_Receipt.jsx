a1f64135826f9fa2ccfcb37db49a6e3fcf55d81b
Alamba <danielalamba15@gmail.com>
2026-03-24 19:00:18 +0100

    fixed school fees session

--- src/components/main/dashboard/documents/Fees_Receipt.jsx
+++ src/components/main/dashboard/documents/Fees_Receipt.jsx
@@ -185,7 +185,7 @@ const Fees_Receipt = () => {
                         <Text strong>Student Name:</Text> {application.surname + ' ' + application.other_names}<br />
                         <Text strong>Matric Number:</Text> {application.matric_number}<br />
                         <Text strong>Department:</Text> {application.course}<br />
-                        <Text strong>Academic Session:</Text> 2024/2025
+                        <Text strong>Academic Session:</Text> 2025/2026
                     </Paragraph>
                     <Divider />
                     <table style={{ width: '100%', borderCollapse: 'collapse', margin: '20px 0' }}>
